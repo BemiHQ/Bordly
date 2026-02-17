5a1c032a30d3cf7cecb95f55b29ddaf69c9dafaa
exAspArk <exaspark@gmail.com>
2026-02-17 13:34:51 -0500

    Configure CSP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,26 @@ import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
-    nitro(),
+    nitro({
+      routeRules: {
+        '/**': {
+          headers: {
+            'Content-Security-Policy': [
+              "default-src 'none'",
+              "script-src 'self' 'unsafe-inline'",
+              "img-src 'self' https:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "frame-src 'none'",
+              `connect-src 'self' ${process.env.VITE_API_ENDPOINT}`,
+              "manifest-src 'self'",
+              "base-uri 'self'",
+              "form-action 'self'",
+            ].join('; '),
+          },
+        },
+      },
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
