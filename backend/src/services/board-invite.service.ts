e108c09dc20da89c8b583e67a2736b8fed21875c
exAspArk <exaspark@gmail.com>
2026-02-17 13:34:42 -0500

    Send email invites

--- backend/src/services/board-invite.service.ts
+++ backend/src/services/board-invite.service.ts
@@ -2,6 +2,8 @@ import type { Populate } from '@mikro-orm/postgresql';
 import { BoardInvite, State } from '@/entities/board-invite';
 import type { User } from '@/entities/user';
 import { BoardService } from '@/services/board.service';
+import { Emailer, NO_REPLY_EMAIL } from '@/utils/emailer';
+import { ENV } from '@/utils/env';
 import { orm } from '@/utils/orm';
 
 export class BoardInviteService {
@@ -31,6 +33,21 @@ export class BoardInviteService {
         return invite;
       });
     await orm.em.flush();
+
+    for (const invite of invites) {
+      await Emailer.send({
+        from: NO_REPLY_EMAIL,
+        to: [invite.email],
+        subject: `You are invited by ${invitedBy.name}`,
+        bodyText: [
+          `${invitedBy.name} has invited you to collaborate on the board "${board.name}". To accept the invitation, please click the link below:`,
+          ``,
+          ENV.APP_ENDPOINT,
+          ``,
+          `If you did not expect this invitation, you can safely ignore this email.`,
+        ].join('\n'),
+      });
+    }
 
     return invites;
   }
---
+++ backend/src/utils/emailer.ts
@@ -0,0 +1,45 @@
+import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
+import { ENV } from '@/utils/env';
+
+export const NO_REPLY_EMAIL = `Bordly <no-reply@${ENV.ROOT_DOMAIN}>`;
+
+export class Emailer {
+  static send({ from, to, subject, bodyText }: { from: string; to: string[]; subject: string; bodyText: string }) {
+    console.log(
+      [
+        'Sending email via AWS SES:',
+        `From: ${from}`,
+        `To: ${to.join(', ')}`,
+        `Subject: ${subject}`,
+        `Body: ${bodyText}`,
+      ].join('\n'),
+    );
+
+    if (!ENV.AWS_REGION || !ENV.AWS_SES_ACCESS_KEY_ID || !ENV.AWS_SES_SECRET_ACCESS_KEY) {
+      return;
+    }
+
+    const client = new SESv2Client({
+      region: ENV.AWS_REGION as string,
+      credentials: {
+        accessKeyId: ENV.AWS_SES_ACCESS_KEY_ID as string,
+        secretAccessKey: ENV.AWS_SES_SECRET_ACCESS_KEY as string,
+      },
+    });
+    const command = new SendEmailCommand({
+      FromEmailAddress: from,
+      Destination: {
+        ToAddresses: to,
+      },
+      Content: {
+        Simple: {
+          Subject: { Data: subject },
+          Body: {
+            Text: { Data: bodyText },
+          },
+        },
+      },
+    });
+    return client.send(command);
+  }
+}
